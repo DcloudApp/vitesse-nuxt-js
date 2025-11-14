eb96700d92d17f757f6cff2005f038c2a4c6ab30
guan9627 <guan.chao.ext@kumu.ph>
2025-11-14 16:37:27 +0800

    ✨ feat: test Deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,11 +57,6 @@ export default defineNuxtConfig({
         target: 'esnext',
       },
     },
-    prerender: {
-      crawlLinks: false,
-      routes: ['/'],
-      ignore: ['/hi'],
-    },
   },
 
   eslint: {
